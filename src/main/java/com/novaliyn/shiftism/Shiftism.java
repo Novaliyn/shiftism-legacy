b4ee0d6b76a543b5c09b3e744a5126cc2dd434fb
novaliyn <189510880+Novaliyn@users.noreply.github.com>
2026-03-12 14:52:58 -0700

    ADDED Crafting

--- src/main/java/com/novaliyn/shiftism/Shiftism.java
+++ src/main/java/com/novaliyn/shiftism/Shiftism.java
@@ -18,6 +18,9 @@ public class Shiftism {
     public static final String VERSION = "1.0";
     public static Item miku_ingot;
     public static Item miku_sword;
+    public static Item miku_rod;
+    public static Item impure_miku_dust;
+    public static Item pure_miku_dust;
     public static final Item.ToolMaterial MIKU = EnumHelper.addToolMaterial("MIKU", 4, 3939, 10f, 6f, 39).setRepairItem(new ItemStack(miku_ingot));
 
 
@@ -27,12 +30,20 @@ public class Shiftism {
         GameRegistry.registerItem(miku_ingot, miku_ingot.getUnlocalizedName().substring(5));
         miku_sword = new ItemSword(MIKU).setUnlocalizedName("miku_sword").setTextureName("shiftism:miku_sword").setCreativeTab(tabShiftism);
         GameRegistry.registerItem(miku_sword, miku_sword.getUnlocalizedName().substring(5));
+        miku_rod = new ItemShiftism().setUnlocalizedName("miku_rod").setTextureName("shiftism:miku_rod").setCreativeTab(tabShiftism);
+        GameRegistry.registerItem(miku_rod, miku_rod.getUnlocalizedName().substring(5));
+        impure_miku_dust = new ItemShiftism().setUnlocalizedName("impure_miku_dust").setTextureName("shiftism:impure_miku_dust").setCreativeTab(tabShiftism);
+        GameRegistry.registerItem(impure_miku_dust, impure_miku_dust.getUnlocalizedName().substring(5));
+        pure_miku_dust = new ItemShiftism().setUnlocalizedName("pure_miku_dust").setTextureName("shiftism:pure_miku_dust").setCreativeTab(tabShiftism);
+        GameRegistry.registerItem(pure_miku_dust, pure_miku_dust.getUnlocalizedName().substring(5));
     }
 
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
+        GameRegistry.addSmelting(pure_miku_dust, new ItemStack(miku_ingot), 3.9f);
+        GameRegistry.addRecipe(new ItemStack(miku_rod), new Object[]{"M", "M", 'M', miku_ingot});
+        GameRegistry.addRecipe(new ItemStack(miku_sword), new Object[]{"M", "M", "R", 'M', miku_ingot, 'R', miku_rod});
     }
 
     @EventHandler
